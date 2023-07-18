4a8a771b5ffec3a03fd766cd5269bbcc168dd20e
Olexander616 <109907687+Olexander616@users.noreply.github.com>
2023-07-18 18:53:20 +0300

    add second activity

--- app/src/main/java/com/example/twoactivityandlist/MainActivity.kt
+++ app/src/main/java/com/example/twoactivityandlist/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.twoactivityandlist
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.constraintlayout.widget.ConstraintLayout
+import java.lang.invoke.ConstantCallSite
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val ConstraintLayout : ConstraintLayout = findViewById(R.id.fon)
+        ConstraintLayout.setOnClickListener{
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
